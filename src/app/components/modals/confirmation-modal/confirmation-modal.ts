fb00476d572ba0dce2d9ade3003d07a8c8dd98eb
sandy <sandydwi75@gmail.com>
2025-09-21 13:44:22 +0700

    feat: slicing appbar

---
+++ src/app/components/modals/confirmation-modal/confirmation-modal.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CustomButton } from '../../custom-button/custom-button';
+
+@Component({
+  selector: 'app-confirmation-modal',
+  imports: [
+    CustomButton
+  ],
+  templateUrl: './confirmation-modal.html',
+  styleUrl: './confirmation-modal.css'
+})
+export class ConfirmationModal {
+  @Input() title: string = '';
+  @Input() message: string = '';
+
+  @Output() cancelAction = new EventEmitter();
+  @Output() confirmAction = new EventEmitter();
+
+  onCancel(): void {
+    this.cancelAction.emit();
+  }
+
+  onConfirm(): void {
+    this.confirmAction.emit();
+  }
+}
